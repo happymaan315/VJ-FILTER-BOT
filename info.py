6912565e449e97a316bdc78748b3ab322b52c33b
happymaan315 <166788448+happymaan315@users.noreply.github.com>
2024-04-14 13:37:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,34 +102,16 @@ LANGUAGES = ["tamil", "english", "hindi","telugu","kannada"]
     
 
 # Online Stream and Download
-NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = None
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
-else:
-    ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://vj-filter-bot-kvzd.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://vj-filter-bot-kvzd.onrender.com/".format(FQDN, PORT)
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
+
+# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'StreamBot'))
-MULTI_CLIENT = False
-name = str(environ.get('name', 'TechVJBot'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
-
 else:
     ON_HEROKU = False
-HAS_SSL=bool(getenv('HAS_SSL',False))
-if HAS_SSL:
-    URL = "https://mytestvj-998c9929dc7a.herokuapp.com/")
-else:
-    URL = "https://mytestvj-998c9929dc7a.herokuapp.com/")
+URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
 
 
 
