8140a6c146e3b6ca6e5b85d5054414b09b3305bb
happymaan315 <166788448+happymaan315@users.noreply.github.com>
2024-04-14 13:17:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,9 +127,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://vj-filter-bot-kvzd.onrender.com",format(FQDN)
+    URL = "https://vj-filter-bot-kvzd.onrender.com/",format(FQDN)
 else:
-    URL = "https://vj-filter-bot-kvzd.onrender.com",format(FQDN)
+    URL = "https://vj-filter-bot-kvzd.onrender.com/",format(FQDN)
 
 
 
